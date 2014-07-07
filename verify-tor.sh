ca6b903961ba690d4fb6f8b21dd2df51532191f3
blufro93 <blufro93@mail2tor.com>
2014-07-07 01:50:06 -0400

    tweaked regex on entry-guards slightly to get al the fingerprints

--- verify-tor.sh
+++ verify-tor.sh
@@ -1,5 +1,5 @@
 #assumes tcpdump and tor-arm packages are installed
 
 echo "GETINFO entry-guards" | arm -p | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" > entry-guards
-cat entry-guards | grep -Eo "[A-F0-9]+\~" | grep -Eo "[A-F0-9]+" > fingerprints
+cat entry-guards | grep -Eo "[A-F0-9]+[\~\=]" | grep -Eo "[A-F0-9]+" > fingerprints
 for i in `cat fingerprints`; do echo "/info $i" | arm -p | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"; done | grep "address" | grep -Eo "[0-9\.]{2,}" | grep "\." > ips
