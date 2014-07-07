8ead96808ab7466aebc9703211903d674fd11f54
blufro93 <blufro93@mail2tor.com>
2014-07-07 02:07:47 -0400

    fixed bug in tail command and added tcpdump command

--- verify-tor.sh
+++ verify-tor.sh
@@ -8,7 +8,7 @@ cat entry-guards | grep -Eo "[A-F0-9]+[\~\=]" | grep -Eo "[A-F0-9]+" > fingerpri
 for i in `cat fingerprints`; do echo "/info $i" | arm -p | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"; done | grep "address" | grep -Eo "[0-9\.]{2,}" | grep "\." > ips
 # construct our tcpdump inerface
 interface="eth0 not host `head -n 1 ips`"
-for i in `tail -n + 2 ips`; do interface="$interface and not host $i"; done
+for i in `tail -n +2 ips`; do interface="$interface and not host $i"; done
 echo $interface
 
 # tcpdump
